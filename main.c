5fafe68363e9ad0b6c2db7595f0fc5c7133a5749
WizStriver1 <1954620922@qq.com>
2019-01-02 23:37:31 +0800

    初步完善

--- main.c
+++ main.c
@@ -2,18 +2,52 @@
 #include <stdlib.h>
 #include <io.h>
 
-int main()
+char *getAbsltPath(char *first, char *last)
+{
+    char *slashes = "\\";
+    char *ap = (char *) malloc(strlen(first) + strlen(slashes) + strlen(last));
+    strcpy(ap, first);
+    strcat(ap, slashes);
+    strcat(ap, last);
+    return ap;
+}
+
+void generateDirTree(char *dirpath)
 {
     long Handle;
     struct _finddata_t FileInfo;
-    if((Handle = _findfirst("*.*", &FileInfo)) == -1L)
+    char findrule[] = "*.*";
+    char *findpath = getAbsltPath(dirpath, findrule);
+    printf("%s--%d\n", dirpath, _A_SUBDIR);
+    if((Handle = _findfirst(findpath, &FileInfo)) == -1L)
         printf("Not found\n");
     else
     {
-        printf("%s\n", FileInfo.name);
         while(_findnext(Handle, &FileInfo) == 0)
-            printf("%s\n", FileInfo.name);
+        {
+            char *recentPath = getAbsltPath(dirpath, FileInfo.name);
+            printf("%s--%u\n", FileInfo.name, FileInfo.attrib);
+            // if((strcmp(FileInfo.name, "..") != 0))
+            // {
+                // if(FileInfo.attrib == _A_SUBDIR)
+                // {
+                //     char *recentDirpath = getAbsltPath(dirpath, &(FileInfo.name));
+                //     generateDirTree(recentDirpath);
+                // }
+                // else
+                // {
+                //     char *recentPath = getAbsltPath(dirpath, &(FileInfo.name));
+                //     printf("%s--%u\n", recentPath, FileInfo.attrib);
+                // }
+            // }
+        }
         _findclose(Handle);
     }
+}
+
+int main(int argc, char *argv[])
+{
+    char dirpath[] = "D:\\DevEnvironment\\CTestDir_Tempspace";
+    generateDirTree(dirpath);
     return 0;
 }
